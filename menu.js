c4eb9d9544f327893a95cf5be02ffa4c5ee3ca16
sprite <ponkaew.wasin@gmail.com>
2021-11-24 16:03:56 -0600

    update

--- menu.js
+++ menu.js
@@ -1,83 +1,66 @@
-// function for downloading file to keep it external
-// Such as creating a json file  
-// Ex. download(json_app, 'appetizers.json', 'text/plain')
-function download(content, fileName, contentType) {
-    const a = document.createElement("a");
-    const file = new Blob([content], { type: contentType });
-    a.href = URL.createObjectURL(file);
-    a.download = fileName;
-    a.click();
-} 
-  
-// making menu objects 
-class Menu {
-constructor(name, description, price) {
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    }
-}
-  
-
-// Attempt at importing menu data from external file
-  
-import { appetizers } from "./menu_data.js";
-
-const app_btn = document.querySelector('.appetizers');
-
-function insert(){
-      
-    // creates menu card with title
-    const menu = document.querySelector('#menu');
-    const menu_card = document.createElement('div');
-    menu_card.classList.add('menu_card');
-
-    const card_category = document.createElement('h1');
-    card_category.textContent = 'Appetizers';
-    menu_card.appendChild(card_category);
+// // Used to read json files
+// function readTextFile(file, callback) {
+//     let rawFile = new XMLHttpRequest();
+//     rawFile.overrideMimeType("application/json");
+//     rawFile.open("GET", file, true);
+//     rawFile.onreadystatechange = function() {
+//         if (rawFile.readyState === 4 && rawFile.status == "200") {
+//             callback(rawFile.responseText);
+//         }
+//     }
+//     rawFile.send(null);
+// }
 
 
-    const food_item = document.createElement('div');
-    food_item.classList.add('food_item');
+// // all menu functions must be done inside function
+// // Appetizers 
+// readTextFile("./menu/appetizers.json", (text) => {
+//     let appetizers = JSON.parse(text);
 
-    const food_top = document.createElement('div');
-    food_top.classList.add('food_item_top');
-    const food_name = document.createElement('h3');
-    food_name.textContent = appetizers[0].name;
+//     const app_btn = document.querySelector('.appetizers');
 
-    const price = document.createElement('div');
-    price.textContent = appetizers[0].price;
+//     const menu_row = document.createElement('div');
+//     menu_row.classList.add('menu_row');
 
-    const food_description = document.createElement('p');
-    food_description.textContent = appetizers[0].description;
+//     function insert_appetizers(){
+//         const menu = document.querySelector('#menu');
+//         // creates menu card with title
+        
+//         const menu_card = document.createElement('div');
+//         menu_card.classList.add('menu_card');
+    
+//         const card_category = document.createElement('h1');
+//         card_category.textContent = 'Appetizers';
+//         menu_card.appendChild(card_category);
+    
 
-    food_top.appendChild(food_name);
-    food_top.appendChild(price);
-    food_item.appendChild(food_top);
-    food_item.appendChild(food_description);
-    menu_card.appendChild(food_item);
-    menu.appendChild(menu_card); 
-}
+//         for (let i=0; i < appetizers.items.length; i++){
+//             const food_item = document.createElement('div');
+//             food_item.classList.add('food_item');
+        
+//             const food_top = document.createElement('div');
+//             food_top.classList.add('food_item_top');
+//             const food_name = document.createElement('h3');
+//             food_name.textContent = appetizers.items[i].name;
+        
+//             const price = document.createElement('div');
+//             price.textContent = appetizers.items[i].price;
+        
+//             const food_description = document.createElement('p');
+//             food_description.textContent = appetizers.items[i].description;
+        
+//             food_top.appendChild(food_name);
+//             food_top.appendChild(price);
+//             food_item.appendChild(food_top);
+//             food_item.appendChild(food_description);
+//             menu_card.appendChild(food_item);
+//         }
 
-app_btn.addEventListener('click', insert);
 
+//         menu_row.appendChild(menu_card);
+//         menu.appendChild(menu_row); 
+//     }
+    
+//     app_btn.addEventListener('click', insert_appetizers);
+// });
 
-
-// Used to read json files
-function readTextFile(file, callback) {
-    let rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-// all menu functions must be done inside function
-readTextFile("./menu/alacarte.json", (text) => {
-    let data = JSON.parse(text);
-    console.log(data.items[0].number);
-});
---
+++ read.js
@@ -0,0 +1,91 @@
+// we need a function to load files
+// done is a "callback" function
+// so you call it once you're finished and pass whatever you want
+// in this case, we're passing the `responseText` of the XML request
+function loadFile(filePath, done) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function () { return done(this.responseText) }
+    xhr.open("GET", filePath, true);
+    xhr.send();
+}
+
+let menu_count = 0
+// paths to all of your files
+const myFiles = [ "./menu/appetizers.json", "./menu/salad.json", "./menu/soup.json" ];
+// where you want to store the data
+const food_data = [];
+
+
+
+// loop through each file
+myFiles.forEach((file, i) => {
+    // and call loadFile
+    // note how a function is passed as the second parameter
+    // that's the callback function
+    loadFile(file, (responseText) => {
+        // we set jsonData[i] to the parse data since the requests
+        // will not necessarily come in order
+        // so we can't use JSONdata.push(JSON.parse(responseText));
+        // if the order doesn't matter, you can use push
+        food_data[i] = JSON.parse(responseText);
+        // or you could choose not to store it in an array.
+        // whatever you decide to do with it, it is available as
+        // responseText within this scope (unparsed!)
+        menu_count += 1;
+
+        if (menu_count == myFiles.length){
+            display();
+        } 
+        
+    })
+});
+
+// All actions must be done in this function
+function display(){
+    const app_btn = document.querySelector('.appetizers');
+
+    const menu_row = document.createElement('div');
+    menu_row.classList.add('menu_row');
+
+    function insert_appetizers(){
+        const menu = document.querySelector('#menu');
+        // creates menu card with title
+        
+        const menu_card = document.createElement('div');
+        menu_card.classList.add('menu_card');
+    
+        const card_category = document.createElement('h1');
+        card_category.textContent = 'Appetizers';
+        menu_card.appendChild(card_category);
+    
+
+        for (let i=0; i < food_data[0].items.length; i++){
+            const food_item = document.createElement('div');
+            food_item.classList.add('food_item');
+        
+            const food_top = document.createElement('div');
+            food_top.classList.add('food_item_top');
+            const food_name = document.createElement('h3');
+            food_name.textContent = food_data[0].items[i].name;
+        
+            const price = document.createElement('div');
+            price.textContent = food_data[0].items[i].price;
+        
+            const food_description = document.createElement('p');
+            food_description.textContent = food_data[0].items[i].description;
+        
+            food_top.appendChild(food_name);
+            food_top.appendChild(price);
+            food_item.appendChild(food_top);
+            food_item.appendChild(food_description);
+            menu_card.appendChild(food_item);
+        }
+
+
+        menu_row.appendChild(menu_card);
+        menu.appendChild(menu_row); 
+    }
+    
+    app_btn.addEventListener('click', insert_appetizers);
+    console.log(food_data[0].items.length)
+}
