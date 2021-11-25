8c3b5756120a09efc644c96ec1cefcc2d43134ec
sprite <ponkaew.wasin@gmail.com>
2021-11-24 20:22:10 -0600

    updated appetizer menu

--- app_menu.js
+++ app_menu.js
@@ -43,23 +43,22 @@ myFiles.forEach((file, i) => {
 // All actions must be done in this function
 function display(){
     const app_btn = document.querySelector('.appetizers');
+    const menu = document.querySelector('#menu');
 
     const menu_row = document.createElement('div');
     menu_row.classList.add('menu_row');
 
     function insert_appetizers(){
-        const menu = document.querySelector('#menu');
         // creates menu card with title
-        
         const menu_card = document.createElement('div');
         menu_card.classList.add('menu_card');
     
         const card_category = document.createElement('h1');
-        card_category.textContent = 'Appetizers';
+        card_category.textContent = food_data[0].name;
         menu_card.appendChild(card_category);
     
 
-        for (let i=0; i < food_data[0].items.length; i++){
+        for (let i=0; i < food_data[0].items.length / 2; i++){
             const food_item = document.createElement('div');
             food_item.classList.add('food_item');
         
@@ -81,11 +80,48 @@ function display(){
             menu_card.appendChild(food_item);
         }
 
-
         menu_row.appendChild(menu_card);
         menu.appendChild(menu_row); 
     }
+
+    // For salad
+    function insert_salad(){
+        const menu_card = document.createElement('div');
+        menu_card.classList.add('menu_card');
+
+    
+        const card_category = document.createElement('h1');
+        card_category.textContent = food_data[0].name;
+        menu_card.appendChild(card_category);
+    
+
+        for (let i=7; i < food_data[0].items.length; i++){
+            const food_item = document.createElement('div');
+            food_item.classList.add('food_item');
+        
+            const food_top = document.createElement('div');
+            food_top.classList.add('food_item_top');
+            const food_name = document.createElement('h3');
+            food_name.textContent = food_data[0].items[i].name;
+        
+            const price = document.createElement('div');
+            price.textContent = food_data[0].items[i].price;
+        
+            const food_description = document.createElement('p');
+            food_description.textContent = food_data[0].items[i].description;
+        
+            food_top.appendChild(food_name);
+            food_top.appendChild(price);
+            food_item.appendChild(food_top);
+            food_item.appendChild(food_description);
+            menu_card.appendChild(food_item);
+        }
+
+        menu_row.appendChild(menu_card);
+        menu.appendChild(menu_row); 
+
+    }
     
     app_btn.addEventListener('click', insert_appetizers);
-    console.log(food_data[0].items.length)
+    app_btn.addEventListener('click', insert_salad);
 }
